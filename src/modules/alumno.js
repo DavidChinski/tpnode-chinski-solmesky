b14e674bbe517a636b0dbf4d10c6fe0d45c66591
Android Studio <devandroid@ort>
2025-03-28 08:57:44 -0300

    ej 1 2 y 3 listo

---
+++ src/modules/alumno.js
@@ -0,0 +1,11 @@
+export class Alumno {
+    constructor(username, dni, edad) {
+        this.username = username || "";
+        this.dni = dni || "";
+        this.edad = edad || 0;
+    }
+
+    toString() {
+        return `Alumno: ${this.username}, DNI: ${this.dni}, Edad: ${this.edad}`;
+    }
+}
---
+++ src/modules/matematica.js
@@ -0,0 +1,20 @@
+/* Este es el módulo "matematicas" */
+const PI = 3.14;
+let array = ["dos","cuatro","ocho","diez"]
+function sumar(x, y) 
+{
+    return x+y;
+}
+function restar(x, y) 
+{
+    return x-y;
+}
+
+const multiplicar = (a, b) => {
+    return a*b;
+};
+const dividir = (a, b) => {
+    return a/b;
+   };
+// Exporto todo lo que yo quiero exponer del módulo hacia el exterior.
+export {PI, sumar, array};
